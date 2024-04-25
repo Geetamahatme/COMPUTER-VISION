b4b43113efabf588656e6bd5d5b6d8a94646be7a
kvishal2706 <21bcs133@iiitdwd.ac.in>
2024-04-25 22:55:19 +0530

    "feat: new features added"

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ cap.set(4, hCam)
 detector = htm.HandDetector(maxHands=1)
 wScreen, hScreen = autopy.screen.size()
 
+last_click_time=time.time()
+
 while True:
     success, image = cap.read()
     image = detector.find_Hands(image)
@@ -34,37 +36,33 @@ while True:
         print(fingers)
         cv2.rectangle(image, (Reduce_Frame, Reduce_Frame), (wCam - Reduce_Frame, hCam - Reduce_Frame),(255, 0, 255), 2)
         
-        if fingers == [0, 1, 0, 0, 0]:
-           
-                x3 = np.interp(x1, (Reduce_Frame+50, wCam - Reduce_Frame-50), (0, wScreen))
-                y3 = np.interp(y1, (Reduce_Frame+50, hCam - Reduce_Frame-50), (0, hScreen))
+        if fingers[1] == 1 and fingers[2] == 0 and fingers[3]==0:
+            x3 = np.interp(x1, (Reduce_Frame+50, wCam - Reduce_Frame-50), (0, wScreen))
+            y3 = np.interp(y1, (Reduce_Frame+50, hCam - Reduce_Frame-50), (0, hScreen))
 
-                curr_x = prev_x+(x3-prev_x)/smoothen
-                curr_y = prev_y+(y3-prev_y)/smoothen
+            curr_x = prev_x+(x3-prev_x)/smoothen
+            curr_y = prev_y+(y3-prev_y)/smoothen
 
-                try:
-                    autopy.mouse.move(wScreen - curr_x, curr_y)
-                except:
-                    print("finger out of range")
-                cv2.circle(image, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-                prev_x,prev_y=curr_x,curr_y
+            try:
+                autopy.mouse.move(wScreen - curr_x, curr_y)
+            except:
+                print("finger out of range")
+            cv2.circle(image, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            prev_x,prev_y=curr_x,curr_y
         
             
-        if fingers[1] == 1 and fingers[2] == 1:
+        if fingers[1] == 1 and fingers[2] == 1 and fingers[3]==0:
             length, image, extraPts = detector.findDistance(8, 12, image)
            # print(length)
             if length < 40:
-                cv2.circle(image, (extraPts[4], extraPts[5]),15, (0, 255, 0), cv2.FILLED)
-                autopy.mouse.click()
+                current_time=time.time()
+                if current_time - last_click_time >= 2:
+                    cv2.circle(image, (extraPts[4], extraPts[5]),15, (0, 255, 0), cv2.FILLED)
+                    autopy.mouse.click()
+                    last_click_time=current_time
           
-       # if fingers == [1, 1, 0, 0, 0]:
-        #    length, image, extraPts = detector.findDistance(4, 8, image)
-         #   print(length)
-          #  if length < 15:
-           #     cv2.circle(image, (extraPts[4], extraPts[5]),15, (0, 255, 0), cv2.FILLED)
-            #    mouse.click('right')
-        if fingers == [0, 0, 1, 0, 0]:
-            mouse.click('right')
+        if fingers == [0, 0, 0, 1, 0]:  # Check if only the index finger is up
+            mouse.click('right')  # Perform a right-click action
             time.sleep(0.5)
 
         if fingers == [0, 1, 1, 1, 0]:  # If three fingers are detected
@@ -84,7 +82,3 @@ while True:
     cv2.imshow("Image", image)
     if cv2.waitKey(25) == ord('q'):
         break
-
-
-
-
--- piano.py
+++ piano.py
@@ -10,6 +10,8 @@ cam=cv2.VideoCapture(0)
 cam.set(3,wCam)
 cam.set(4,hCam)
 
+pTime = 0
+
 pygame.mixer.init()
 pygame.mixer.set_num_channels(17)
 effect1=pygame.mixer.Sound('./piano_sound/key01.mp3')
@@ -37,6 +39,7 @@ width=65
 start=[5,70,145,220,295,370,445,520,595,670,745,820,895,970,1045,1120,1195]  #17
 end=[60,135,210,285,360,435,510,585,660,735,810,885,960,1035,1110,1185,1260]
 
+last_click_time=time.time()
 
 while True:
     ret,frame=cam.read()
@@ -58,67 +61,73 @@ while True:
             x1=start[i]
             x2=end[i]
 
-            print(fList[8][1])
+            # print(fList[8][1])
 
             if x1<=fList[8][1]<=x2 and 0<=fList[8][2]<=260:
                 cv2.rectangle(frame, (x1,0), (x2,250), (255,0,255),1)
 
                 length, image, extraPts = detector.findDistance(8, 12, frame)
-                print(length)
-                if length < 35:
+                # print(length)
+                if length < 50:
+                    curr_time=time.time()
                     cv2.rectangle(frame, (x1,0), (x2,250), (0,0,255),1)
-
-                    if i == 0:
-                        effect1.play()
-                        # effect1.fadeout(1000)
-                    elif i == 1:
-                        effect2.play()
-                        # effect2.fadeout(1000)
-                    elif i == 2:
-                        effect3.play()
-                        # effect3.fadeout(1000)
-                    elif i == 3:
-                        effect4.play()
-                        # effect4.fadeout(1000)
-                    elif i == 4:
-                        effect5.play()
-                        # effect5.fadeout(1000)
-                    elif i == 5:
-                        effect6.play()
-                        # effect6.fadeout(1000)
-                    elif i == 6:
-                        effect7.play()
-                        # effect7.fadeout(1000)
-                    elif i == 7:
-                        effect8.play()
-                        # effect8.fadeout(1000)
-                    elif i == 8:
-                        effect9.play()
-                        # effect9.fadeout(1000)
-                    elif i == 9:
-                        effect10.play()
-                        # effect10.fadeout(1000)
-                    elif i == 10:
-                        effect11.play()
-                        # effect11.fadeout(1000)
-                    elif i == 11:
-                        effect12.play()
-                        # effect12.fadeout(1000)
-                    elif i == 12:
-                        effect13.play()
-                        # effect13.fadeout(1000)
-                    elif i == 13:
-                        effect14.play()
-                        # effect14.fadeout(1000)
-                    elif i == 14:
-                        effect15.play()
-                        # effect15.fadeout(1000)
-                    elif i == 15:
-                        effect16.play()
-                        # effect16.fadeout(1000)
-                    elif i==16:
-                        effect17.play()
-            
+                    
+                    if curr_time-last_click_time>=0.5:
+                        if i == 0:
+                            effect1.play()
+                            # effect1.fadeout(1000)
+                        elif i == 1:
+                            effect2.play()
+                            # effect2.fadeout(1000)
+                        elif i == 2:
+                            effect3.play()
+                            # effect3.fadeout(1000)
+                        elif i == 3:
+                            effect4.play()
+                            # effect4.fadeout(1000)
+                        elif i == 4:
+                            effect5.play()
+                            # effect5.fadeout(1000)
+                        elif i == 5:
+                            effect6.play()
+                            # effect6.fadeout(1000)
+                        elif i == 6:
+                            effect7.play()
+                            # effect7.fadeout(1000)
+                        elif i == 7:
+                            effect8.play()
+                            # effect8.fadeout(1000)
+                        elif i == 8:
+                            effect9.play()
+                            # effect9.fadeout(1000)
+                        elif i == 9:
+                            effect10.play()
+                            # effect10.fadeout(1000)
+                        elif i == 10:
+                            effect11.play()
+                            # effect11.fadeout(1000)
+                        elif i == 11:
+                            effect12.play()
+                            # effect12.fadeout(1000)
+                        elif i == 12:
+                            effect13.play()
+                            # effect13.fadeout(1000)
+                        elif i == 13:
+                            effect14.play()
+                            # effect14.fadeout(1000)
+                        elif i == 14:
+                            effect15.play()
+                            # effect15.fadeout(1000)
+                        elif i == 15:
+                            effect16.play()
+                            # effect16.fadeout(1000)
+                        elif i==16:
+                            effect17.play()
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    print(fps)
+    cv2.putText(img, str(int(fps)), (20,550), cv2.FONT_HERSHEY_PLAIN, 3,(255, 0, 0), 3)      
     cv2.imshow("Frame",img)
     
     if cv2.waitKey(25) == ord('q'):
